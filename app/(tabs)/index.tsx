db42478bca46f4b765bd0b3ba478c5aa0a230645
Sergej Garkusha <Tenebrann@googlemail.com>
2024-12-13 17:28:44 +0100

    Initial commit: Initialize DayTrack-Native project with React Native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,77 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import Checkbox from 'expo-checkbox';
 
 export default function HomeScreen() {
+  const [value, setValue] = useState('TextInput');
+
+  const [tasks, setTasks] = useState([
+    { id: 1, title: 'HTML', isDone: true },
+    { id: 2, title: 'JS', isDone: false },
+    { id: 3, title: 'CSS', isDone: true },
+    { id: 4, title: 'React', isDone: true },
+    { id: 5, title: 'ReactNative', isDone: false },
+  ]);
+
+  const taskStatusChangeHandler = (id: number) => {
+    setTasks(tasks.map((list) => (list.id === id ? { ...list, isDone: !list.isDone } : list)));
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <TextInput
+        style={[globalStyles.border, styles.input]}
+        value={value}
+        onChangeText={setValue}
+      />
+      <View style={styles.boxContainer}>
+        {tasks.map((task) => {
+          return (
+            <View key={task.id} style={[styles.boxTask, globalStyles.border]}>
+              <Checkbox
+                value={task.isDone}
+                onValueChange={() => taskStatusChangeHandler(task.id)}></Checkbox>
+              <Text>{task.title}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#232946',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input: {
+    width: '60%',
+    backgroundColor: '#fffffe',
+    borderColor: 'black',
+    fontSize: 20,
+    padding: 5,
+    marginBottom: 15,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  boxTask: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+
+  boxContainer: {
+    width: '60%',
+  },
+});
+
+const globalStyles = StyleSheet.create({
+  border: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'black',
   },
 });
